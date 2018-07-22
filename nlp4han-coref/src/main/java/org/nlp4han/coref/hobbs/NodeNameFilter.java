83197f2b5ea45af91d0831ec2d6811a006fc3ee6
whyzc <35511503+whyzc@users.noreply.github.com>
2018-07-22 16:03:32 +0800

    添加Filter类

---
+++ nlp4han-coref/src/main/java/org/nlp4han/coref/hobbs/NodeNameFilter.java
@@ -0,0 +1,71 @@
+package org.nlp4han.coref.hobbs;
+
+import java.util.List;
+
+import com.lc.nlp4han.constituent.TreeNode;
+
+public class NodeNameFilter implements Filter
+{
+	private List<TreeNode> treeNodes;
+	private String[] treeNodeName;
+	
+	public NodeNameFilter()
+	{
+		
+	}
+
+	public NodeNameFilter(List<TreeNode> treeNodes)
+	{
+		this.treeNodes = treeNodes;
+	}
+
+	public NodeNameFilter(List<TreeNode> treeNodes, String[] treeNodeName)
+	{
+		super();
+		this.treeNodes = treeNodes;
+		this.treeNodeName = treeNodeName;
+	}
+
+	public void setTreeNodes(List<TreeNode> treeNodes)
+	{
+		this.treeNodes = treeNodes;
+	}
+
+	public void setTreeNodeName(String[] treeNodeName)
+	{
+		this.treeNodeName = treeNodeName;
+	}
+
+	@Override
+	public List<TreeNode> filtering()
+	{
+		if (treeNodeName != null)
+		{
+			for (int i = 0; i < treeNodes.size(); i++)
+			{
+				boolean flag = false;
+				for (int j = 0; j < treeNodeName.length; j++)
+				{
+					if (treeNodeName[j].equals(treeNodes.get(i).getNodeName()))
+					{
+						flag = true;
+						break;
+					}
+				}
+				if (flag)
+					break;
+				treeNodes.remove(i);
+				i--;
+			}
+		}
+		return treeNodes;
+	}
+
+	@Override
+	public void setUp(List<TreeNode> treeNodes)
+	{
+		this.treeNodes = treeNodes;
+		
+	}
+
+}
