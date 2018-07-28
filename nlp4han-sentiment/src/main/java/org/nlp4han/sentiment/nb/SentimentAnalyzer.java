b0537349381a97fc0128912d5f164a00e2c76a56
lincham <1377973989@qq.com>
2018-07-28 11:47:13 +0800

    删除多余文件

--- nlp4han-sentiment/src/main/java/org/nlp4han/sentiment/nb/SentimentAnalyzer.java
+++ nlp4han-sentiment/src/main/java/org/nlp4han/sentiment/nb/SentimentAnalyzer.java
@@ -2,33 +2,25 @@ package org.nlp4han.sentiment.nb;
 
 import java.util.Map;
 
-public interface SentimentAnalyzer {
+public interface SentimentAnalyzer
+{
 	/**
-	   * Analyze the given text provided as tokens along with
-	   * the provided extra information
-	   *
-	   * @param text the tokens of text to categorize
-	   * @param extraInformation extra information
-	   * @return per category probabilities
-	   */
-	  SentimentPolarity analyze(String text, Map<String, Object> extraInformation);
+	 * 分析给定文本的情感极性
+	 *
+	 * @param text
+	 *            待分析的文本
+	 * @param 附加信息
+	 * @return 情感极性
+	 */
+	SentimentPolarity analyze(String text, Map<String, Object> extraInformation);
 
-	  /**
-	   * Analyze the given text, provided in separate tokens.
-	   * @param text the tokens of text to categorize
-	   * @return per category probabilities
-	   */
-	  SentimentPolarity analyze(String text);
-
-
-
-	  /**
-	   * get the number of categories
-	   *
-	   * @return the no. of categories
-	   */
-	  int getNumberOfCategories();
-
-
+	/**
+	 * 分析给定文本的情感信息
+	 * 
+	 * @param text
+	 *            给定的文本
+	 * @return 情感极性
+	 */
+	SentimentPolarity analyze(String text);
 
 }
