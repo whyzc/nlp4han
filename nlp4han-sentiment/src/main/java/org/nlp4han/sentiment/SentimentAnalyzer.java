cf6e79700cfdfa36e86249e39d325865ff7159fb
lincham <1377973989@qq.com>
2018-07-31 15:46:28 +0800

    修改相关文件的层次结构

---
+++ nlp4han-sentiment/src/main/java/org/nlp4han/sentiment/SentimentAnalyzer.java
@@ -0,0 +1,26 @@
+package org.nlp4han.sentiment;
+
+import java.util.Map;
+
+public interface SentimentAnalyzer
+{
+	/**
+	 * 分析给定文本的情感极性
+	 *
+	 * @param text
+	 *            待分析的文本
+	 * @param 附加信息
+	 * @return 情感极性
+	 */
+	SentimentPolarity analyze(String text, Map<String, Object> extraInformation);
+
+	/**
+	 * 分析给定文本的情感信息
+	 * 
+	 * @param text
+	 *            给定的文本
+	 * @return 情感极性
+	 */
+	SentimentPolarity analyze(String text);
+
+}
