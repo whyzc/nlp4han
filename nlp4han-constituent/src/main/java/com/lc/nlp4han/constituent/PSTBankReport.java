cb63f7eb43b4c3244f55314ef97e85b590199e3c
qyl <qyl@DESKTOP-L6M7N94>
2018-08-22 16:02:11 +0800

    1.PSTBankReport修改 2.交叉验证工具类改名字

--- nlp4han-constituent/src/main/java/com/lc/nlp4han/constituent/PSTBankReport.java
+++ nlp4han-constituent/src/main/java/com/lc/nlp4han/constituent/PSTBankReport.java
@@ -13,15 +13,14 @@ public class PSTBankReport
 	private HashSet<String> wordShapeSet;
 	private int sentenceCount;// 句子数
 	private int wordCount;// 总字数(包括标点符号)
-	private int nonTerminalCount;// 非终结符的个数
-
+	private HashSet<String> nonTerminalSet;//非终结符集合
 	public PSTBankReport()
 	{
 		tokenCount = 0;
 		wordShapeSet = new HashSet<String>();
 		sentenceCount = 0;
 		wordCount = 0;
-		nonTerminalCount = 0;
+		nonTerminalSet=new HashSet<String>();
 	}
 
 	/**
@@ -54,7 +53,7 @@ public class PSTBankReport
 		int meanLevelOfTree = heightTatleCount / sentenceCount;
 		ptbt.close();
 		// 括号表达式生成文法
-		return new TreeBankReport(tokenCount, wordShapeSet.size(), sentenceCount, wordCount, nonTerminalCount, heighest,
+		return new TreeBankReport(tokenCount, wordShapeSet.size(), sentenceCount, wordCount, nonTerminalSet.size(), heighest,
 				lowest, meanLevelOfTree);
 	}
 
@@ -70,7 +69,7 @@ public class PSTBankReport
 		}
 		else
 		{
-			nonTerminalCount++;
+			nonTerminalSet.add(node.nodename);
 		}
 		int height = 0;
 		for (TreeNode node1 : node.getChildren())
