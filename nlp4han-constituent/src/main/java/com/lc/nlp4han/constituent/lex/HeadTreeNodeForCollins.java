3e81e76039f722924863aa5be5092f0c055cf66f
qyl <qyl@windows10.microdone.cn>
2018-09-18 21:20:32 +0800

    1.补充

--- nlp4han-constituent/src/main/java/com/lc/nlp4han/constituent/lex/HeadTreeNodeForCollins.java
+++ nlp4han-constituent/src/main/java/com/lc/nlp4han/constituent/lex/HeadTreeNodeForCollins.java
@@ -4,29 +4,34 @@ import com.lc.nlp4han.constituent.HeadTreeNode;
 
 public class HeadTreeNodeForCollins extends HeadTreeNode
 {
-    private boolean verb=false;//该节点是否包含动词
-    private int headChildIndex;//该节点的headChild的索引值
+	private boolean verb = false;// 该节点是否包含动词
+	private int headChildIndex;// 该节点的headChild的索引值
+
 	public HeadTreeNodeForCollins(String nodename)
 	{
 		super(nodename);
 	}
+
 	public boolean isVerb()
 	{
 		return verb;
 	}
+
 	public void setVerb(boolean verb)
 	{
 		this.verb = verb;
 	}
+
 	public int getHeadChildIndex()
 	{
 		return headChildIndex;
 	}
+
 	public void setHeadChildIndex(int headChildIndex)
 	{
 		this.headChildIndex = headChildIndex;
 	}
-	
+
 	/**
 	 * 带有头结点、动词属性、以及headChild的索引值树的输出（一行括号表达式）
 	 */
@@ -35,14 +40,14 @@ public class HeadTreeNodeForCollins extends HeadTreeNode
 	{
 		if (super.children.size() == 0)
 		{
-			String str=super.BracketConvert(this.nodename);
+			String str = super.BracketConvert(this.nodename);
 			return " " + str + "[" + this.getWordIndex() + "]";
 		}
 		else
 		{
 			String treestr = "";
-			treestr = "(" + super.BracketConvert(this.nodename) + "{" + super.BracketConvert(super.getHeadWord()) 
-			+ "[" + super.getHeadPos() + " "+this.verb+" "+this.headChildIndex+"]}";
+			treestr = "(" + super.BracketConvert(this.nodename) + "{" + super.BracketConvert(super.getHeadWord()) + "["
+					+ super.getHeadPos() + " " + this.verb + " " + this.headChildIndex + "]}";
 			for (HeadTreeNode node : getChildren())
 			{
 				treestr += node.toString();
@@ -51,6 +56,7 @@ public class HeadTreeNodeForCollins extends HeadTreeNode
 			return treestr;
 		}
 	}
+
 	@Override
 	public boolean equals(Object obj)
 	{
@@ -64,5 +70,5 @@ public class HeadTreeNodeForCollins extends HeadTreeNode
 			return false;
 		}
 	}
-	
+
 }
