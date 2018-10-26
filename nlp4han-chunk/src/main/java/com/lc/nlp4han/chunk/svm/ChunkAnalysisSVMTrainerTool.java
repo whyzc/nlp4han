ff9b5bca029d82a9fb64d8bf8c0d5ba2319c2edd
whyzc <35511503+whyzc@users.noreply.github.com>
2018-10-26 10:27:03 +0800

    修改特征生成方式等

--- nlp4han-chunk/src/main/java/com/lc/nlp4han/chunk/svm/ChunkAnalysisSVMTrainerTool.java
+++ nlp4han-chunk/src/main/java/com/lc/nlp4han/chunk/svm/ChunkAnalysisSVMTrainerTool.java
@@ -2,7 +2,6 @@ package com.lc.nlp4han.chunk.svm;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,10 +11,34 @@ public class ChunkAnalysisSVMTrainerTool
 	
 	
 	public static final String USAGE = "Usage: ChunkAnalysisSVMTrainerTool [options] -data training_set_file\n" 
-			+ "options:\n"
-			+ "-encoding encoding : set encoding" 
-			+ "-label label : such as BIOE, BIOES"
-			+ SVMTrain.OPTIONS;
+			+"options:\n"
+			+"-encoding encoding : set encoding" 
+			+"-label label : such as BIOE, BIOES"
+			+"-s svm_type : set type of SVM (default 0)\n"
+			+"	0 -- C-SVC		(multi-class classification)\n"
+			+"	1 -- nu-SVC		(multi-class classification)\n"
+			+"	2 -- one-class SVM\n"
+			+"	3 -- epsilon-SVR	(regression)\n"
+			+"	4 -- nu-SVR		(regression)\n"
+			+"-t kernel_type : set type of kernel function (default 2)\n"
+			+"	0 -- linear: u'*v\n"
+			+"	1 -- polynomial: (gamma*u'*v + coef0)^degree\n"
+			+"	2 -- radial basis function: exp(-gamma*|u-v|^2)\n"
+			+"	3 -- sigmoid: tanh(gamma*u'*v + coef0)\n"
+			+"	4 -- precomputed kernel (kernel values in training_set_file)\n"
+			+"-d degree : set degree in kernel function (default 3)\n"
+			+"-g gamma : set gamma in kernel function (default 1/num_features)\n"
+			+"-r coef0 : set coef0 in kernel function (default 0)\n"
+			+"-c cost : set the parameter C of C-SVC, epsilon-SVR, and nu-SVR (default 1)\n"
+			+"-n nu : set the parameter nu of nu-SVC, one-class SVM, and nu-SVR (default 0.5)\n"
+			+"-p epsilon : set the epsilon in loss function of epsilon-SVR (default 0.1)\n"
+			+"-m cachesize : set cache memory size in MB (default 100)\n"
+			+"-e epsilon : set tolerance of termination criterion (default 0.001)\n"
+			+"-h shrinking : whether to use the shrinking heuristics, 0 or 1 (default 1)\n"
+			+"-b probability_estimates : whether to train a SVC or SVR model for probability estimates, 0 or 1 (default 0)\n"
+			+"-wi weight : set the parameter C of class i to weight*C, for C-SVC (default 1)\n"
+			+"-v n : n-fold cross validation mode\n"
+			+"-q : quiet mode (no outputs)\n";
 	
 	
 	public static void main(String[] args) throws IOException
@@ -24,19 +47,20 @@ public class ChunkAnalysisSVMTrainerTool
 		Map<String, String[]> as = decompositionArgs(args);
 		
 		String[] inputArgs = as.get("input");
-		SVMStandardInput.main(inputArgs);
+		SVMStandardInput ssi = SVMStandardInput.run(inputArgs);
 		
-		//System.out.println("样本总数：" + input.length);
-		System.out.println("类别总数：" + SVMStandardInput.getClassificationResults().size());
+		List<Integer> numberOfClassification = ssi.getNumberOfClassification();
+		System.out.println("类别总数：" + numberOfClassification.size());
+		long total = 0;
+		for (int n : numberOfClassification)
+			total += n;
+		System.out.println("样本总数：" + total);
 		
 		String[] trainArgs = as.get("train");
 		
-		trainArgs = addWeight(trainArgs, SVMStandardInput.getNumberOfClassification());
-		//t.run(trainArgs, input, true, SVMStandardInput.getScaleInfo());
-		//List l = SVMStandardInput.getNumberOfClassification();
+		//trainArgs = addWeight(trainArgs, SVMStandardInput.getNumberOfClassification());
 		svm_train.main(trainArgs);
 
-		
 		long endTime = System.currentTimeMillis();
 		System.out.println("共耗时：" + (endTime-startTime)*1.0/60000 + "mins");
 	}
@@ -234,9 +258,11 @@ public class ChunkAnalysisSVMTrainerTool
 	//y = 1- (c-1)*(x-min)/[c*(max-min)]
 	private static double calculation(int x, int min, int max, double c)
 	{
-		int p = 1000;		// c/p 为最小惩罚项
+		double p = 1.0*max/min*0.1;		// c/p 为最小惩罚项
+		//double p = 100;		// c/p 为最小惩罚项
 		double result = 0;
-		result = 1 - 1.0*(p-1)*(Math.sqrt(x)-Math.sqrt(min))/(p)/(Math.sqrt(max) - Math.sqrt(min));
+		//result = 1 - 1.0*(p-1)*(Math.sqrt(x)-Math.sqrt(min))/(p)/(Math.sqrt(max) - Math.sqrt(min));
+		result = 1.0 - (p-1)*(x-min)/(p)/(max - min);
 		return result;
 	}
 	
