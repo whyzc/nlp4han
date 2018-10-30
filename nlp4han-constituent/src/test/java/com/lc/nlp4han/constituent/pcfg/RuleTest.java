5c0d872c799a9877bee7b2892e933c6a11c5a4e2
liuxf <kiddenliu@sina.com>
2018-10-30 10:01:02 +0800

    增加PRule的构造函数单元测试。

--- nlp4han-constituent/src/test/java/com/lc/nlp4han/constituent/pcfg/RuleTest.java
+++ nlp4han-constituent/src/test/java/com/lc/nlp4han/constituent/pcfg/RuleTest.java
@@ -11,9 +11,12 @@ public class RuleTest
 	public void testConstructFromStr()
 	{
 		String ruleStr1 = "A->B c";
-		RewriteRule r1 = new RewriteRule(ruleStr1);
-		
+		RewriteRule r1 = new RewriteRule(ruleStr1);	
 		assertEquals(ruleStr1, r1.toString());
+		
+		String ruleStr2 = "A->B c ---- 0.123";
+		RewriteRule r2 = new RewriteRule(ruleStr2);
+		assertEquals(ruleStr2, r2.toString());
 	}
 
 }
