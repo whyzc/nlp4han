b27d45efebdef980374270c22c647faa15cd9849
liuxf <kiddenliu@sina.com>
2019-01-02 11:33:37 +0800

    重构lex解析代码。

--- nlp4han-constituent/src/main/java/com/lc/nlp4han/constituent/lex/LexGrammarExtractor.java
+++ nlp4han-constituent/src/main/java/com/lc/nlp4han/constituent/lex/LexGrammarExtractor.java
@@ -199,8 +199,10 @@ public class LexGrammarExtractor
 		// 在解析中需要用此得到候选的父节点，以免遍历不需要的非终结符
 		OccurenceHeadChild rhcg0 = new OccurenceHeadChild(headLabel, null, headpos, headword);
 		addParents(rhcg0, parentLabel);
+		
 		OccurenceHeadChild rhcg = new OccurenceHeadChild(headLabel, null, headpos, null);
 		addParents(rhcg, parentLabel);
+		
 		OccurenceHeadChild rhcg1 = new OccurenceHeadChild(headLabel, null, null, null);
 		addParents(rhcg1, parentLabel);
 	}
@@ -240,12 +242,13 @@ public class LexGrammarExtractor
 		// 单独取左侧生成规则
 		for (int i = headIndex - 1; i >= 0; i--)
 		{
-			getOneSideGR(1, i, parentLabel, headLabel, headPOS, headWord, headIndex, node);
+			addOneSideGR(1, i, parentLabel, headLabel, headPOS, headWord, headIndex, node);
 		}
+		
 		// 单独取右侧的生成规则
 		for (int i = headIndex + 1; i < node.getChildrenNum(); i++)
 		{
-			getOneSideGR(2, i, parentLabel, headLabel, headPOS, headWord, headIndex, node);
+			addOneSideGR(2, i, parentLabel, headLabel, headPOS, headWord, headIndex, node);
 		}
 
 	}
@@ -262,6 +265,7 @@ public class LexGrammarExtractor
 		String parentLabel = node.getNodeName();
 		String headWord = node.getHeadWord();
 		String headPOS = node.getHeadPos();
+		
 		Distance leftDistance = getDistance(node, 1, headIndex, -1);// 左侧距离
 		Distance rightDistance = getDistance(node, 2, headIndex, node.getChildrenNum());// 右侧距离
 
@@ -270,16 +274,16 @@ public class LexGrammarExtractor
 			headWord = node.getFirstChildHeadWord();
 			headPOS = node.getFirstChildHeadWordPos();
 			headLabel = node.getFirstChildName();
-			getOneSideStopGRule(true, parentLabel, headLabel, headPOS, headWord, new Distance(), 1);// 左侧 Stop符号
+			addOneSideStopGRule(true, parentLabel, headLabel, headPOS, headWord, new Distance(), 1);// 左侧 Stop符号
 			headWord = node.getLastChildHeadWord();
 			headPOS = node.getLastChildHeadPos();
 			headLabel = node.getLastChildName();
-			getOneSideStopGRule(true, parentLabel, headLabel, headPOS, headWord, new Distance(), 2);// 右侧Stop符号
+			addOneSideStopGRule(true, parentLabel, headLabel, headPOS, headWord, new Distance(), 2);// 右侧Stop符号
 			return;
 		}
 
-		getOneSideStopGRule(true, parentLabel, headLabel, headPOS, headWord, leftDistance, 1);// 左侧 Stop符号
-		getOneSideStopGRule(true, parentLabel, headLabel, headPOS, headWord, rightDistance, 2);// 右侧Stop符号
+		addOneSideStopGRule(true, parentLabel, headLabel, headPOS, headWord, leftDistance, 1);// 左侧 Stop符号
+		addOneSideStopGRule(true, parentLabel, headLabel, headPOS, headWord, rightDistance, 2);// 右侧Stop符号
 	}
 
 	/**
@@ -294,12 +298,13 @@ public class LexGrammarExtractor
 	 * @param headWord
 	 * @param headIndex
 	 */
-	private void getOneSideGR(int direction, int i, String parentLabel, String headLabel, String headPOS,
+	private void addOneSideGR(int direction, int i, String parentLabel, String headLabel, String headPOS,
 			String headWord, int headIndex, HeadTreeNodeForCollins node)
 	{
 		String sideLabel = node.getChildName(i);// 所求孩子节点的标记
 		String sideHeadPOS = node.getChildHeadPos(i);// 所求孩子节点的中心词词标记
 		String sideHeadWord = node.getChildHeadWord(i);// 所求的孩子节点的中心词
+		
 		boolean coor = false;// 并列结构,0为不设值，1和2为有或者没有
 		boolean pu = false;// 标点符号，由于只保留了顿号所以我们可以把它当做并列结构，并列结构,0为不设值，1和2为有或者没有
 		Distance distance = getDistance(node, direction, headIndex, i);
@@ -324,7 +329,7 @@ public class LexGrammarExtractor
 			}
 		}
 		// 在此处虽然不生成Stop但仍要统计其数据，在计算概率时使用
-		getOneSideStopGRule(false, parentLabel, headLabel, headPOS, headWord, distance, direction);
+		addOneSideStopGRule(false, parentLabel, headLabel, headPOS, headWord, distance, direction);
 
 		// 并列结构处理
 		if (processCoorPU)
@@ -332,7 +337,7 @@ public class LexGrammarExtractor
 			if ((i >= 1 && node.getChildName(i).equals("CC")) && node.getNodeName().equals(node.getChildName(i - 1))
 					&& node.getNodeName().equals(node.getChildName(i + 1)))
 			{
-				getOneSideGROfCoor(direction, i, parentLabel, headLabel, headPOS, headWord, headIndex, node);
+				addOneSideGROfCoor(direction, i, parentLabel, headLabel, headPOS, headWord, headIndex, node);
 				return;
 			}
 			else if (i >= 2 && node.getChildName(i - 1).equals("CC"))
@@ -340,6 +345,7 @@ public class LexGrammarExtractor
 				coor = true;
 			}
 		}
+		
 		// 生成两侧Label和pos的回退模型
 		// 回退模型1
 		OccurenceSides rsg0 = new OccurenceSides(headLabel, parentLabel, headPOS, headWord, direction, sideLabel,
@@ -392,7 +398,7 @@ public class LexGrammarExtractor
 	 * @param headIndex
 	 * @param node
 	 */
-	private void getOneSideGROfCoor(int direction, int i, String parentLabel, String headLabel, String headPOS,
+	private void addOneSideGROfCoor(int direction, int i, String parentLabel, String headLabel, String headPOS,
 			String headWord, int headIndex, HeadTreeNodeForCollins node)
 	{
 		// 若为并列结构，也单独处理
@@ -434,16 +440,18 @@ public class LexGrammarExtractor
 	 * @param distance
 	 * @param direction
 	 */
-	private void getOneSideStopGRule(boolean stop, String parentLabel, String headLabel, String headPOS,
+	private void addOneSideStopGRule(boolean stop, String parentLabel, String headLabel, String headPOS,
 			String headWord, Distance distance, int direction)
 	{
 		// 生成两侧word的回退模型,输出只有两种，所以不需要单独添加计算概率和权重时的分母`
 		// 回退模型1
 		OccurenceStop sg0 = new OccurenceStop(headLabel, parentLabel, headPOS, headWord, direction, stop, distance);
 		addGenerateRule(sg0, null, stopGenMap);
+		
 		// 回退模型2
 		OccurenceStop sg2 = new OccurenceStop(headLabel, parentLabel, headPOS, null, direction, stop, distance);
 		addGenerateRule(sg2, null, stopGenMap);
+		
 		// 回退模型3
 		OccurenceStop sg4 = new OccurenceStop(headLabel, parentLabel, null, null, direction, stop, distance);
 		addGenerateRule(sg4, null, stopGenMap);
@@ -468,6 +476,7 @@ public class LexGrammarExtractor
 			distance.setAdjacency(true);
 			return distance;
 		}
+		
 		// 非邻接
 		int start, end;
 		if (direction == 1)
@@ -480,6 +489,7 @@ public class LexGrammarExtractor
 			start = headIndex + 1;
 			end = i - 1;
 		}
+		
 		for (int j = start; j <= end; j++)
 		{
 			HeadTreeNodeForCollins collinsNode = (HeadTreeNodeForCollins) node.getChild(j);
@@ -489,6 +499,7 @@ public class LexGrammarExtractor
 				return distance;
 			}
 		}
+		
 		return distance;
 	}
 
@@ -496,43 +507,44 @@ public class LexGrammarExtractor
 	 * 添加头结点的生成规则 在计算概率时，rule1指定子节点具体信息的规则，而rule2对应于做了平滑处理的规则（即回退，也就是子节点某处信息为null）
 	 * rule1不需要计算子类型数目
 	 * 
-	 * @param rule1
-	 * @param rule2
+	 * @param specificOccur
+	 * @param generalOccur
 	 */
-	private void addGenerateRule(OccurenceCollins rule1, OccurenceCollins rule2,
+	private void addGenerateRule(OccurenceCollins specificOccur, OccurenceCollins generalOccur,
 			HashMap<OccurenceCollins, RuleAmountsInfo> map)
 	{
-		if (rule2 == null)
-		{// 生成两侧stop时，不需要计算回退规则，故rule2为空
-			if ((!map.containsKey(rule1)))
+		if (generalOccur == null)
+		{// 生成两侧stop时，不需要计算回退规则，故generalOccur为空
+			if ((!map.containsKey(specificOccur)))
 			{
-				map.put(rule1, new RuleAmountsInfo(1, 0));
+				map.put(specificOccur, new RuleAmountsInfo(1, 0));
 			}
 			else
 			{
-				map.get(rule1).addAmount(1);
+				map.get(specificOccur).addAmount(1);
 			}
 			return;
 		}
 
-		if (!map.containsKey(rule2))
+		if (!map.containsKey(generalOccur))
 		{// 回退规则不存在
-			map.put(rule1, new RuleAmountsInfo(1, 0));
-			map.put(rule2, new RuleAmountsInfo(1, 1));
+			map.put(specificOccur, new RuleAmountsInfo(1, 0));
+			map.put(generalOccur, new RuleAmountsInfo(1, 1));
 		}
 		else
 		{
-			if (!map.containsKey(rule1))
+			if (!map.containsKey(specificOccur))
 			{
-				map.put(rule1, new RuleAmountsInfo(1, 0));
-				map.get(rule2).addSubtypeAmount(1);// rule1不存在，则意味着回退模型rule2的子类型数目需要+1
+				map.put(specificOccur, new RuleAmountsInfo(1, 0));
+				map.get(generalOccur).addSubtypeAmount(1);// specificOccur不存在，则意味着回退模型generalOccur的子类型数目需要+1
 			}
 			else
 			{
-				map.get(rule1).addAmount(1);
+				map.get(specificOccur).addAmount(1);
 			}
-			// 不管rule1是否已经存在，回退规则rule2的数目都要+1
-			map.get(rule2).addAmount(1);
+			
+			// 不管specificOccur是否已经存在，回退规则generalOccur的数目都要+1
+			map.get(generalOccur).addAmount(1);
 		}
 	}
 }
