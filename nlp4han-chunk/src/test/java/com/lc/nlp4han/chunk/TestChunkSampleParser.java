bec234d15efe802784a7ef6e2c9a0332ab8c3366
liuxf <kiddenliu@sina.com>
2019-01-03 15:52:40 +0800

    组块样本解析单元测试类改名；增加BIEO单元测试。

--- nlp4han-chunk/src/test/java/com/lc/nlp4han/chunk/TestChunkSampleParser.java
+++ nlp4han-chunk/src/test/java/com/lc/nlp4han/chunk/TestChunkSampleParser.java
@@ -5,12 +5,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.lc.nlp4han.chunk.wordpos.ChunkerWordPosParserBIEO;
 import com.lc.nlp4han.chunk.wordpos.ChunkerWordPosParserBIO;
 
-public class TestChunkAnalysisMeasureBIO
+public class TestChunkSampleParser
 {
 	@Test
-	public void testParse_0()
+	public void testParseBIO()
 	{
 		ChunkerWordPosParserBIO cwppBIO = new ChunkerWordPosParserBIO();
 		
@@ -23,16 +24,6 @@ public class TestChunkAnalysisMeasureBIO
 		assertEquals(7, tokens.length);
 		assertEquals(7, tags.length);
 		assertEquals(7, Context.length);
-	}
-	
-	@Test
-	public void testParse_1()
-	{
-		ChunkerWordPosParserBIO cwppBIO = new ChunkerWordPosParserBIO();
-		
-		AbstractChunkAnalysisSample acas = cwppBIO.parse("[上海/NR 浦东/NR]NP [开发/NN 与/CC 法制/NN 建设/NN]NP [同步/VV]VP");
-		
-		String[] tokens = acas.getTokens();
 		
 		boolean flag = false;
 		for (String t : tokens)
@@ -42,18 +33,8 @@ public class TestChunkAnalysisMeasureBIO
 		}
 		
 		assertTrue(flag);
-	}
-	
-	@Test
-	public void testParse_2()
-	{
-		ChunkerWordPosParserBIO cwppBIO = new ChunkerWordPosParserBIO();
 		
-		AbstractChunkAnalysisSample acas = cwppBIO.parse("[上海/NR 浦东/NR]NP [开发/NN 与/CC 法制/NN 建设/NN]NP [同步/VV]VP");
-		
-		String[] tags = acas.getTags();
-		
-		boolean flag = false;
+		flag = false;
 		for (String t : tags)
 		{
 			if ("VP_B".equals(t))
@@ -62,4 +43,39 @@ public class TestChunkAnalysisMeasureBIO
 		
 		assertTrue(flag);
 	}
+	
+	@Test
+	public void testParseBIEO()
+	{
+		ChunkerWordPosParserBIEO cwppBIO = new ChunkerWordPosParserBIEO();
+		
+		AbstractChunkAnalysisSample acas = cwppBIO.parse("[上海/NR 浦东/NR]NP [开发/NN 与/CC 法制/NN 建设/NN]NP [同步/VV]VP");
+		
+		String[] tokens = acas.getTokens();
+		String[] tags = acas.getTags();
+		Object[] Context = acas.getAditionalContext();
+		
+		assertEquals(7, tokens.length);
+		assertEquals(7, tags.length);
+		assertEquals(7, Context.length);
+		
+		boolean flag = false;
+		for (String t : tokens)
+		{
+			if ("同步".equals(t))
+				flag = true;
+		}
+		
+		assertTrue(flag);
+		
+		flag = false;
+		for (String t : tags)
+		{
+			if ("VP_B".equals(t))
+				flag = true;
+		}
+		
+		assertTrue(flag);
+	}
+	
 }
