d5fa0352453a5eaef40b6b30a2d85aa550e3a8fc
liuxf <kiddenliu@sina.com>
2019-01-08 13:45:56 +0800

    给从文件获得Text添加IOException异常。

--- nlp4han-clustering/src/main/java/com/lc/nlp4han/clustering/Text.java
+++ nlp4han-clustering/src/main/java/com/lc/nlp4han/clustering/Text.java
@@ -9,19 +9,18 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-
 public class Text
 {
 	private static int defaultName = 1;
 	private String name;
 	private String content;
 	private Sample sample;
-	
+
 	public Text()
 	{
-		
+
 	}
-	
+
 	public Text(String name, String content)
 	{
 		this.name = name;
@@ -30,7 +29,7 @@ public class Text
 
 	public Text(File f)
 	{
-		
+
 	}
 
 	public Text(String content)
@@ -42,41 +41,53 @@ public class Text
 
 	/**
 	 * 从磁盘路径中加载Text
-	 * @param folderPath 文件路径
-	 * @param useDefaultName 是否使用默认名作为Text名，若为false，会将文件的文件名作为Text.name
+	 * 
+	 * @param folderPath
+	 *            文件路径
+	 * @param useDefaultName
+	 *            是否使用默认名作为Text名，若为false，会将文件的文件名作为Text.name
 	 * @return Text的列表
+	 * @throws IOException
 	 */
-	public static List<Text> getTexts(String folderPath, boolean useDefaultName)
+	public static List<Text> getTexts(String folderPath, boolean useDefaultName) throws IOException
 	{
 		return getTexts(folderPath, useDefaultName, "UTF-8");
 	}
-	
+
 	/**
-	 从磁盘路径中加载Text
-	 * @param folderPath 文件路径
-	 * @param useDefaultName 是否使用默认名作为Text名，若为false，会将文件的文件名作为Text.name
-	 * @param encoding 文件的编码方式
+	 * 从磁盘路径中加载Text
+	 * 
+	 * @param folderPath
+	 *            文件路径
+	 * @param useDefaultName
+	 *            是否使用默认名作为Text名，若为false，会将文件的文件名作为Text.name
+	 * @param encoding
+	 *            文件的编码方式
 	 * @return Text的列表
+	 * @throws IOException
 	 */
-	public static List<Text> getTexts(String folderPath, boolean useDefaultName, String encoding)
+	public static List<Text> getTexts(String folderPath, boolean useDefaultName, String encoding) throws IOException
 	{
 		List<Text> result = new ArrayList<Text>();
-		List<File> files = travaringFilesForLevelTraversal(folderPath);
-		
+		List<File> files = getFiles(folderPath);
+
 		for (File file : files)
 		{
-			Text t = processingFile(file, useDefaultName, encoding);
+			Text t = getText(file, useDefaultName, encoding);
 			if (t != null)
 				result.add(t);
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * 生成Sample
-	 * @param sg 样本生成器
-	 * @param fg 特征生成器
+	 * 
+	 * @param sg
+	 *            样本生成器
+	 * @param fg
+	 *            特征生成器
 	 */
 	public void generateSample(FeatureGenerator fg)
 	{
@@ -92,7 +103,7 @@ public class Text
 	public void setSample(Sample s)
 	{
 		this.sample = s;
-		
+
 	}
 
 	public String getName()
@@ -104,26 +115,26 @@ public class Text
 	{
 		return content;
 	}
-	
+
 	/**
 	 * 层次遍历pos文件夹下所有文件
-	 * @param pos 待遍历的文件夹
+	 * 
+	 * @param path
+	 *            待遍历的文件夹
 	 * @return 文件夹pos下的所有文件列表
+	 * @throws IOException
 	 */
-	private static List<File> travaringFilesForLevelTraversal(String pos)
+	private static List<File> getFiles(String path) throws IOException
 	{
 		List<File> files = new LinkedList<File>();
 		List<File> result = new LinkedList<File>();
-		File tmpFile = new File(pos);
-		if (pos == null)
-		{
-			throw new RuntimeException("地址为空");
-		}
+		File tmpFile = new File(path);
+		if (path == null)
+			throw new IOException("地址为空");
+
 		if (!tmpFile.exists())
-		{
-			throw new RuntimeException("文件地址不存在");
-		}
-		
+			throw new IOException("文件地址不存在");
+
 		files.add(tmpFile);
 		while (!files.isEmpty())
 		{
@@ -136,63 +147,54 @@ public class Text
 			else if (tmpFile.isDirectory())
 			{
 				subFiles = tmpFile.listFiles();
-				for (int i=0 ; i<subFiles.length ; i++)
-				{
+				for (int i = 0; i < subFiles.length; i++)
 					files.add(subFiles[i]);
-				}
 			}
 		}
-		
+
 		return result;
-		
+
 	}
 
-	
 	/**
 	 * 处理文档
-	 * @param f 待处理的文档
-	 * @param useDefaultName 是否使用默认名字
-	 * @param texts 存储生成的文本层文本
+	 * 
+	 * @param f
+	 *            待处理的文档
+	 * @param useDefaultName
+	 *            是否使用默认名字
+	 * @param texts
+	 *            存储生成的文本层文本
+	 * @throws IOException 
 	 */
-	private static Text processingFile(File f, boolean useDefaultName, String encoding)
+	private static Text getText(File f, boolean useDefaultName, String encoding) throws IOException
 	{
-		
+
 		StringBuffer content = new StringBuffer();
 		String tmp;
 		BufferedReader bufr;
-		try
-		{
-			bufr = new BufferedReader(new InputStreamReader(new FileInputStream(f), encoding));
-		
-			while ((tmp=bufr.readLine()) != null)
-			{
-				content.append(tmp);
-			}
-			bufr.close();
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	
+
+		bufr = new BufferedReader(new InputStreamReader(new FileInputStream(f), encoding));
+
+		while ((tmp = bufr.readLine()) != null)
+			content.append(tmp);
+
+		bufr.close();
+
 		if (content.length() > 0)
 		{
 			Text t = null;
 			if (useDefaultName || f.getName() == null || f.getName() == "")
-			{
 				t = new Text(content.toString());
-			}
 			else
-			{
 				t = new Text(f.getName(), content.toString());
-			}
-				
+
 			return t;
 		}
 		else
 			return null;
-		
-	}
 
+	}
 
 	@Override
 	public String toString()
@@ -236,5 +238,5 @@ public class Text
 			return false;
 		return true;
 	}
-	
+
 }
