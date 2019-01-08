c7bb22f3fa43c6997029e633349437f62c12f12e
liuxf <kiddenliu@sina.com>
2019-01-08 13:58:20 +0800

    整理聚类代码格式。

--- nlp4han-clustering/src/main/java/com/lc/nlp4han/clustering/KMeans.java
+++ nlp4han-clustering/src/main/java/com/lc/nlp4han/clustering/KMeans.java
@@ -5,66 +5,63 @@ import java.util.List;
 public class KMeans
 {
 	private static final int TIMES = 5000;
-	
+
 	public static List<Group> run(List<Text> texts, int k)
 	{
-		if (texts==null || k<1 || texts.size()<k)
+		if (texts == null || k < 1 || texts.size() < k)
 			throw new RuntimeException("参数错误！");
- 
+
 		FeatureGenerator fg = new WordBasedZeroOneFeatureGenerator();
 		if (!fg.isInitialized())
 			fg.init(texts);
-		
+
 		DistanceCalculator distance = new JaccardCoefficientBasedDistanceCalculator();
-		
-		for (int i=0 ; i<texts.size() ; i++)
+
+		for (int i = 0; i < texts.size(); i++)
 		{
 			Text t = texts.get(i);
 			Sample s = new Sample(fg.getFeatures(t));
 			t.setSample(s);
 		}
-		
+
 		DistanceRecode dr = new DistanceRecode(texts, distance);
-		
+
 		UpdateGroupCenter ugc = new PAMUpateGroupCenter(dr);
-		
-		
+
 		Initialization init = new RandomInitialization();
 		List<Group> groups = init.initialize(texts, k);
-		
-		
+
 		int iterationTimes = 0;
-		for (; iterationTimes<TIMES ; iterationTimes++)
+		for (; iterationTimes < TIMES; iterationTimes++)
 		{
-			for (int j=0 ; j<k ; j++)
+			for (int j = 0; j < k; j++)
 			{
-					groups.get(j).clear();
+				groups.get(j).clear();
 			}
-			
-			for (int j=0 ; j<texts.size() ; j++)
+
+			for (int j = 0; j < texts.size(); j++)
 			{
 				int index = minDistanceGroup(texts.get(j), groups, distance);
 				groups.get(index).addMember(texts.get(j));
 			}
-			
-			
+
 			if (!ugc.updateCenter(groups))
 				break;
-			
+
 		}
-		
+
 		if (iterationTimes > TIMES)
-			System.out.println("共迭代："+ (iterationTimes-1) + "次");
+			System.out.println("共迭代：" + (iterationTimes - 1) + "次");
 		else
-			System.out.println("共迭代："+ iterationTimes + "次");
+			System.out.println("共迭代：" + iterationTimes + "次");
 		return groups;
 	}
-	
+
 	private static int minDistanceGroup(Text t, List<Group> grps, DistanceCalculator d)
 	{
 		double min = Double.POSITIVE_INFINITY;
 		int index = -1;
-		for (int i=0 ; i<grps.size() ; i++)
+		for (int i = 0; i < grps.size(); i++)
 		{
 			double distance = d.getDistance(t, grps.get(i));
 			if (distance < min)
@@ -82,5 +79,5 @@ public class KMeans
 		}
 		return index;
 	}
-	
+
 }
